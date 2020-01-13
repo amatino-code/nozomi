c733dec438016c75886e1a7dd5033ef13b33cc62
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-01-13 15:17:38 +1100

    expand error capabilities

--- nozomi/errors/error.py
+++ nozomi/errors/error.py
@@ -3,8 +3,12 @@ Nozomi
 Nozomi rror Module
 Copyright Amatino Pty Ltd
 """
+import datetime
 from nozomi.http.status_code import HTTPStatusCode
+from nozomi.http.headers import Headers
 from nozomi.ancillary.immutable import Immutable
+from nozomi.data.encodable import Encodable
+from typing import Optional, Any, Dict
 import traceback
 
 
@@ -36,3 +40,50 @@ class NozomiError(Exception):
     http_status_code = Immutable(lambda s: s._http)
     client_description = Immutable(lambda s: s._client_description)
     stack_trace = Immutable(lambda s: s._stack_trace)
+    info_package = Immutable(lambda s: s._info_package())
+
+    def _info_package(self) -> Dict[str, Any]:
+        """
+        Return a package of information an application can parse to
+        learn more about the error
+        """
+        data = {
+            'error-information': self.client_description,
+            'response-code': self.http_status_code
+        }
+        return NozomiError._InfoPackage(data)
+
+    def report(
+        self,
+        request_headers: Optional[Headers] = None,
+        request_json: Optional[Any] = None,
+        request_arguments: Optional[Any] = None
+    ) -> str:
+        """
+        Return human-readable a string describing the error, including
+        a traceback
+        """
+        report = '\n\n--##-- Procuret API Error Report --##--\n'
+        report += str(datetime.datetime.utcnow()) + ' UTC\n'
+        report += 'Exception: ' + str(self) + '\n'
+        report += '--//-- Begin traceback --//--\n\n'
+        trace = traceback.format_tb(self.__traceback__)
+        for line in trace:
+            report += line
+        report += '\n--//-- End traceback --//--\n'
+        report += '\nRequest headers: \n'
+        report += str(request_headers)
+        report += '\nRequest JSON: \n'
+        report += str(request_json)
+        report += '\nRequest arguments:\n'
+        report += str(request_arguments)
+        report += '\n--##-- End Error Report --##--\n'
+        return report
+
+    class _InfoPackage(Encodable):
+        def __init__(self, data: Dict[str, Any]) -> None:
+            self._data = data
+            return
+
+        def encode(self) -> Dict[str, Any]:
+            return self._data
