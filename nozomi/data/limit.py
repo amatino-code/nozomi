9b5809018d87eeac37eb202c7c95382fe3148825
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-03-14 15:04:55 +1100

    add optional `Limit` / `Offset` parsing

--- nozomi/data/limit.py
+++ nozomi/data/limit.py
@@ -6,7 +6,7 @@ author: hugh@blinkybeach.com
 from nozomi.http.query_string import QueryString
 from nozomi.data.sql_conforming import SQLConforming
 from nozomi.ancillary.immutable import Immutable
-from typing import TypeVar, Type
+from typing import TypeVar, Type, Optional
 
 T = TypeVar('T', bound='Limit')
 
@@ -36,3 +36,26 @@ class Limit(SQLConforming):
         )
 
         return cls(magnitude)
+
+    @classmethod
+    def optionally_from_arguments(
+        cls: Type[T],
+        arguments: QueryString,
+        max_value: int = 20,
+        min_value: int = 0,
+        fallback_value: Optional[int] = 0
+    ) -> Optional[T]:
+
+        magnitude = arguments.optionally_parse_int(
+            key='limit',
+            max_value=max_value,
+            min_value=min_value
+        )
+
+        if magnitude is not None:
+            return cls(magnitude)
+
+        if fallback_value is None:
+            return None
+
+        return cls(fallback_value)
--- nozomi/data/offset.py
+++ nozomi/data/offset.py
@@ -6,7 +6,7 @@ author: hugh@blinkybeach.com
 from nozomi.http.query_string import QueryString
 from nozomi.data.sql_conforming import SQLConforming
 from nozomi.ancillary.immutable import Immutable
-from typing import TypeVar, Type
+from typing import TypeVar, Type, Optional
 
 T = TypeVar('T', bound='Offset')
 
@@ -34,3 +34,24 @@ class Offset(SQLConforming):
         )
 
         return cls(magnitude)
+
+    @classmethod
+    def optionally_from_arguments(
+        cls: Type[T],
+        arguments: QueryString,
+        min_value: int = 0,
+        fallback_value: Optional[int] = 0
+    ) -> Optional[T]:
+
+        magnitude = arguments.optionally_parse_int(
+            key='offset',
+            min_value=min_value
+        )
+
+        if magnitude is not None:
+            return cls(magnitude)
+
+        if fallback_value is None:
+            return None
+
+        return cls(fallback_value)
