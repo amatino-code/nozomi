225e712fe2a4873d802a45b33335cfe635155d43
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-04-04 16:29:45 +1100

    add `Codable` protocol

---
+++ nozomi/data/codable.py
@@ -0,0 +1,11 @@
+"""
+Nozomi
+Codable Module
+author: hugh@blinkybeach.com
+"""
+from nozomi.data.encodable import Encodable
+from nozomi.data.decodable import Decodable
+
+
+class Codable(Encodable, Decodable):
+    pass
