f001169ed6d2f9cdee8a989a951f7f8e045b4b26
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-04-05 13:04:00 +1000

    add missing `Codable` import

--- nozomi/__init__.py
+++ nozomi/__init__.py
@@ -14,6 +14,7 @@ from nozomi.data.encodable import Encodable
 from nozomi.data.format import Format
 from nozomi.data.encoder import Encoder
 from nozomi.data.decodable import Decodable
+from nozomi.data.codable import Codable
 from nozomi.data.query import Query
 from nozomi.data.index_sql_conforming import IndexSQLConforming
 from nozomi.data.limit import Limit
