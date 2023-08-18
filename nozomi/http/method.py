ac93de79d67a0bd5dee5a62240353ac314c1d190
hwjeremy <hugh.jeremy@gmail.com>
2023-08-18 14:51:26 +1000

    add `HTTMethod` default initialiser with special UNKNOWN case

--- nozomi/http/method.py
+++ nozomi/http/method.py
@@ -4,6 +4,9 @@ HTTPMethod Module
 Copyright Amatino Pty Ltd
 """
 from enum import Enum
+from typing import TypeVar, Type, Any
+
+Self = TypeVar('Self', bound='HTTPMethod')
 
 
 class HTTPMethod(Enum):
@@ -17,3 +20,9 @@ class HTTPMethod(Enum):
     HEAD = 'HEAD'
     CONNECT = 'CONNECT'
     TRACE = 'TRACE'
+
+    UNKNOWN = 'UNKNOWN'
+
+    @classmethod
+    def _missing_(Self: Type[Self], value: Any) -> Self:
+        return Self.UNKNOWN
